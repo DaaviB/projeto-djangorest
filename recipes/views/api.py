fe66ab94f001dec827d79fb742d222540a157e2e
daaviB <britodavi122@gmail.com>
2022-07-09 19:28:28 -0300

    removed browseable api

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
@@ -21,6 +23,7 @@ class RecipeApiV2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RcipeApiV2Pagination
     permission_classes = [IsAuthenticatedOrReadOnly, ]
+    renderer_classes = [JSONRenderer, ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -30,6 +33,16 @@ class RecipeApiV2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers)
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
