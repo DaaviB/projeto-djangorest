d05224742b60a29d6ca5024fe378861d1d63f5c4
daaviB <britodavi122@gmail.com>
2022-07-13 20:51:22 -0300

    test: testing if the user can create a recipe without being logged in

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -6,8 +6,12 @@ from rest_framework.test import APITestCase
 
 
 class RecipeApiV2Test(APITestCase, RecipeMixin):
-    def get_recipe_api_list(self):
-        api_url = reverse('recipes:recipes-api-list')
+    def get_recipe_reverse_url(self, reverse_result=None):
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
+        return api_url
+
+    def get_recipe_api_list(self, reverse_result=None):
+        api_url = self.get_recipe_reverse_url(reverse_result=None)
         response = self.client.get(api_url)
         return response
 
@@ -32,3 +36,33 @@ class RecipeApiV2Test(APITestCase, RecipeMixin):
         recipes = response.data.get('results')
 
         self.assertEqual(len(recipes), 0)
+
+    def test_recipe_api_list_load_recipes_by_category_id(self):
+        # Creates 10 recipes
+        recipes = self.make_recipe_in_batch(qtd=10)
+        # Creates categories
+        wanted_category = self.make_category(name='WANTED_CATEGORY')
+        not_wanted_category = self.make_category(name='NOT_WANTED_CATEGORY')
+        # Change all recipes to the wanted category
+        for recipe in recipes:
+            recipe.category = wanted_category
+            recipe.save()
+        # change one recipe to the NOT wanted category
+        # As a result, this recipe should NOT show in the page
+        recipes[0].category = not_wanted_category
+        recipes[0].save()
+        # Action: get the recipes by wanted category id
+        api_url = reverse('recipes:recipes-api-list') + \
+            f'?category_id={wanted_category.id}'
+        response = self.client.get(api_url)
+
+        recipes_with_wanted_category = len(response.data.get('results'))
+        # We should only see recipes from wanted category
+        print(response.data)
+        print(wanted_category.id)
+        self.assertEqual(recipes_with_wanted_category, 9)
+
+    def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
+        api_url = self.get_recipe_reverse_url()
+        response = self.client.post(api_url)
+        self.assertEqual(response.status_code, 401)
