8e7f5e528d7bbed88832f9cea1a9afd04af4a595
daaviB <britodavi122@gmail.com>
2022-07-12 21:11:52 -0300

    test: testing whether the recipe is shown when it is not published.

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -6,18 +6,29 @@ from rest_framework.test import APITestCase
 
 
 class RecipeApiV2Test(APITestCase, RecipeMixin):
+    def get_recipe_api_list(self):
+        api_url = reverse('recipes:recipes-api-list')
+        response = self.client.get(api_url)
+        return response
+
     def test_recipe_api_list_returns_status_code_200(self):
-        url = reverse('recipes:recipes-api-list')
-        response = self.client.get(url)
+        response = self.get_recipe_api_list()
         self.assertEqual(response.status_code, 200)
 
     @patch('recipes.views.api.RcipeApiV2Pagination.page_size', new=7)
     def test_recipe_api_list_loads_correct_number_of_recipes(self):
         wanted_number_of_recipes = 7
-        url = reverse('recipes:recipes-api-list')
         self.make_recipe_in_batch(wanted_number_of_recipes)
 
-        response = self.client.get(url)
+        response = self.get_recipe_api_list()
         qtd_of_loaded_recipes = len(response.data.get('results'))
 
         self.assertEqual(wanted_number_of_recipes, qtd_of_loaded_recipes)
+
+    def test_recipe_api_list_dont_show_not_published_recipes(self):
+        self.make_recipe(is_published=False, title='I am NOT PUBLISHED')
+
+        response = self.get_recipe_api_list()
+        recipes = response.data.get('results')
+
+        self.assertEqual(len(recipes), 0)
